693506f4af9a7b0e83f55e0381f727e60b4b9fb5
Flavio Alvarenga <flavio.fgjj@live.com>
2022-08-06 16:33:07 -0300

    keyboard number + hide keyboard function added

--- app/src/main/java/com/example/alcoholorgasoline/MainActivity.kt
+++ app/src/main/java/com/example/alcoholorgasoline/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.alcoholorgasoline
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,6 +19,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun hideKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val hideKey = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            hideKey.hideSoftInputFromWindow(view.windowToken, 0)
+        } else {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+        }
+    }
+
     private fun calcBestPrice(_alcoholPrice: String, _gasolinePrice: String) {
         val alcoholPrice = _alcoholPrice.toDouble()
         val gasolinePrice = _gasolinePrice.toDouble()
@@ -23,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         val i = result.text.indexOf(":")
         val res = if ((alcoholPrice / gasolinePrice) < 0.7) "ALCOHOL" else "GASOLINE"
 
-        if (i < 0) "The best option is: ".also { result.text = it }
+        if (!result.text.contains(":")) "The best option is: ".also { result.text = it }
         "${result.text.substring(0, i + 1)} $res".also { result.text = it }
     }
 
@@ -32,10 +45,11 @@ class MainActivity : AppCompatActivity() {
         val gasolinePrice = gasoline_price.text.toString()
 
         if(alcoholPrice.isEmpty() || gasolinePrice.isEmpty()) {
-            "Fill all fields!".also { result.text = it }
+            "All fields need to be filled!".also { result.text = it }
             return
         }
 
         calcBestPrice(alcoholPrice, gasolinePrice)
+        hideKeyboard()
     }
 }
